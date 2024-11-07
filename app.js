69d60fd25c48af3fd2664c3f25f59444cb0bf902
danibengozi <160952396+danibengozi@users.noreply.github.com>
2024-11-07 17:22:27 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ function criaCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container')
     let cartao= document.createElement('article')
     cartao.className=('cartao')
-    cartao.innerHTML=
+
+    cartao.innerHTML=`
     <div class="cartao__conteudo"> 
-        <h3>Programação</h3>
-        <div class="cartao__conteudo__pergunta">9
-        <p>O que é JavaScrit?</p>
+
+        <h3>${Categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+        <p>${Pergunta}</p>
      </div>
      <div class="cartao__conteudo__resposta">
-       <p>O JavaScript é uma linguagem de programação</p></div>     
+       <p>${Resposta}</p>
+       </div>     
+       </div>
+       `
+       container.appendChild(cartao)
 }
